862d701cc055f4a149479b90da5eb18ada15d096
wert-rar <72702040+wert-rar@users.noreply.github.com>
2024-05-18 00:10:15 +0300

    Moved js and html to separate files

---
+++ Client/client.js
@@ -0,0 +1,252 @@
+// TODO:
+// fix bug with wrong offset
+// Make requests to server
+// Write TT
+
+let CANVAS = null;
+let CTX = null;
+let SELECTED_PIECE = null;
+// settings
+let cell_size = 25;
+let steps = ["Ход белых", "Ход черных"];
+let status = {
+  w1: "Ход белых",
+  b1: "Ход черных",
+  w2: "Нельзя двигать фигуру, сейчас ход белых",
+  b2: "Нельзя двигать фигуру, сейчас ход черных",
+  w3: "Победили белые",
+  b3: "Победили черные",
+};
+let colors = {
+  1: "rgb(0,0,0)",
+  0: "rgb(255,255,255)",
+};
+let b_colors = {
+  1: "#DA7422",
+  0: "#FFFBDB",
+};
+
+let pieces = [
+  { color: 1, x: 1, y: 0, mode: "p" },
+  { color: 1, x: 3, y: 0, mode: "p" },
+  { color: 1, x: 5, y: 0, mode: "p" },
+  { color: 1, x: 7, y: 0, mode: "p" },
+  { color: 1, x: 0, y: 1, mode: "p" },
+  { color: 1, x: 2, y: 1, mode: "p" },
+  { color: 1, x: 4, y: 1, mode: "p" },
+  { color: 1, x: 6, y: 1, mode: "p" },
+  { color: 1, x: 1, y: 2, mode: "p" },
+  { color: 1, x: 3, y: 2, mode: "p" },
+  { color: 1, x: 5, y: 2, mode: "p" },
+  { color: 1, x: 7, y: 2, mode: "p" },
+  { color: 0, x: 0, y: 7, mode: "p" },
+  { color: 0, x: 2, y: 7, mode: "p" },
+  { color: 0, x: 4, y: 7, mode: "p" },
+  { color: 0, x: 6, y: 7, mode: "p" },
+  { color: 0, x: 1, y: 6, mode: "p" },
+  { color: 0, x: 3, y: 6, mode: "p" },
+  { color: 0, x: 5, y: 6, mode: "p" },
+  { color: 0, x: 7, y: 6, mode: "p" },
+  { color: 0, x: 0, y: 5, mode: "p" },
+  { color: 0, x: 2, y: 5, mode: "p" },
+  { color: 0, x: 4, y: 5, mode: "p" },
+  { color: 0, x: 6, y: 5, mode: "p" },
+];
+
+// function win(stat) {
+//   if (stat == "b3")
+//     document.getElementsByClassName("board")[0].innerHTML =
+//       "<div class='win'> Победили черные</div>";
+//   else
+//     document.getElementsByClassName("board")[0].innerHTML =
+//       "<div class='win'> Победили белые</div>";
+// }
+
+function onLoad() {
+  CANVAS = document.getElementById("board");
+  CTX = CANVAS.getContext("2d");
+  adjustScreen();
+  update();
+  addEventListeners();
+}
+
+// DRAG AND DROP CODE
+function addEventListeners() {
+  CANVAS.addEventListener("mousedown", onMouseDown);
+  CANVAS.addEventListener("mousemove", onMouseMove);
+  CANVAS.addEventListener("mouseup", onMouseUp);
+  CANVAS.addEventListener("touchstart", onTouchStart);
+  CANVAS.addEventListener("touchmove", onTouchMove);
+  CANVAS.addEventListener("touchend", onTouchEnd);
+}
+
+function onMouseDown(evt) {
+  SELECTED_PIECE = getPressedPiece(evt);
+}
+
+function onMouseMove(evt) {
+  if (SELECTED_PIECE != null) {
+    SELECTED_PIECE.x = evt.x;
+    SELECTED_PIECE.y = evt.y;
+  }
+}
+
+function onMouseUp(evt) {
+  if (SELECTED_PIECE != null) {
+    console.log("server request");
+    let coords = getCoordinates(evt);
+    SELECTED_PIECE.piece.x = coords.x;
+    SELECTED_PIECE.piece.y = coords.y;
+    SELECTED_PIECE = null;
+  }
+}
+
+function onTouchStart(evt) {
+  let loc = {
+    x: evt.touches[0].clientX,
+    y: evt.touches[0].clientY,
+  };
+  onMouseDown(loc);
+}
+
+function onTouchMove(evt) {
+  let loc = {
+    x: evt.touches[0].clientX,
+    y: evt.touches[0].clientY,
+  };
+  onMouseMove(loc);
+}
+
+function onTouchEnd(evt) {
+  let loc = {
+    x: evt.touches[0].clientX,
+    y: evt.touches[0].clientY,
+  };
+  onMouseUp(loc);
+}
+
+function getCoordinates(loc) {
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      if (
+        (i + 1) * cell_size < loc.y &&
+        loc.y < (i + 1) * cell_size + cell_size
+      ) {
+        if (
+          (j + 1) * cell_size < loc.x &&
+          loc.x < (j + 1) * cell_size + cell_size
+        ) {
+          return { x: j, y: i };
+        }
+      }
+    }
+  }
+  return { x: -1, y: -1 };
+}
+
+function getPressedPiece(loc) {
+  let coords = getCoordinates(loc);
+  console.log(coords);
+  for (let i = 0; i < pieces.length; i++) {
+    if (pieces[i].x == coords.x && pieces[i].y == coords.y)
+      return { piece: pieces[i], x: loc.x, y: loc.y };
+  }
+
+  return null;
+}
+
+// DRAG AND DROP CODE
+
+// RENDER CODE
+function adjustScreen() {
+  CANVAS.width = window.innerWidth;
+  CANVAS.height = window.innerHeight * 0.95;
+  cell_size = CANVAS.height * 0.1;
+  console.log(cell_size);
+}
+
+function draw_circle(x, y, r, width, strokeColor, fillColor) {
+  CTX.beginPath();
+  CTX.arc(x, y, r, 0, 2 * Math.PI, false);
+  if (fillColor) {
+    CTX.fillStyle = fillColor;
+    CTX.fill();
+  }
+  if (strokeColor) {
+    CTX.strokeStyle = strokeColor;
+    CTX.lineWidth = width;
+    CTX.stroke();
+  }
+}
+
+function draw_piece(piece) {
+  let fillStyle = colors[piece.color];
+  let strokeStyle = colors[piece.color ? 0 : 1];
+  const X = cell_size * (piece.x + 1.5);
+  const Y = cell_size * (piece.y + 1.5);
+  const radius = (cell_size / 2) * 0.8;
+  draw_circle(X, Y, radius, 3, strokeStyle, fillStyle);
+  draw_circle(X, Y, radius * 0.7, 3, strokeStyle, false);
+
+  if (piece.mode != "p") {
+    draw_circle(X, Y, radius * 0.5, 6, "gold");
+  }
+}
+
+function draw_SELECTED_PIECE() {
+  let fillStyle = colors[SELECTED_PIECE.piece.color];
+  let strokeStyle = colors[SELECTED_PIECE.piece.color ? 0 : 1];
+  const X = SELECTED_PIECE.x;
+  const Y = SELECTED_PIECE.y;
+  const radius = (cell_size / 2) * 0.8;
+  draw_circle(X, Y, radius, 3, strokeStyle, fillStyle);
+  draw_circle(X, Y, radius * 0.7, 3, strokeStyle, false);
+
+  if (SELECTED_PIECE.piece.mode != "p") {
+    draw_circle(X, Y, radius * 0.5, 6, "gold");
+  }
+}
+
+function render_Board() {
+  document.getElementById("status").innerHTML = status["w1"];
+  document.getElementById("step").innerHTML = steps[0];
+
+  CTX.fillStyle = "white";
+  CTX.fillRect(0, 0, CANVAS.width, CANVAS.height);
+
+  CTX.fillStyle = "black";
+  CTX.lineWidth = 2;
+  CTX.strokeRect(cell_size, cell_size, cell_size * 8, cell_size * 8);
+
+  let step = 0;
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      CTX.fillStyle = b_colors[step % 2];
+      CTX.fillRect(
+        cell_size + cell_size * j,
+        cell_size + cell_size * i,
+        cell_size,
+        cell_size
+      );
+      step++;
+    }
+    step++;
+  }
+}
+
+function render_Pieces() {
+  for (let i = 0; i < pieces.length; i++) {
+    if (SELECTED_PIECE?.piece == pieces[i]) {
+      continue;
+    }
+    draw_piece(pieces[i]);
+  }
+  if (SELECTED_PIECE) draw_SELECTED_PIECE();
+}
+
+function update() {
+  render_Board();
+  render_Pieces();
+  window.requestAnimationFrame(update);
+}
+// RENDER CODE END
