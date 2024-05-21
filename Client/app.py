63f4a0b7870e1485340b7755f8fb33384ba39c5f
Христофор <168688308+J0muty@users.noreply.github.com>
2024-05-22 01:45:54 +0300

    Moved the app.py file from Server to Client

--- Client/app.py
+++ Client/app.py
@@ -2,15 +2,17 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+current_player = "white"
+pieces = []
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "Hello World!"
 
 
 @app.route("/board")
 def get_board():
-    return render_template("board.html")
+    return render_template('board.html')
 
 
 @app.route("/move", methods=["POST"])
@@ -24,7 +26,8 @@ def move():
     current_player = "black" if current_player == "white" else "white"
 
     response = {"status": "ok",
-                "current_player": current_player},
+                "current_player": current_player,
+                "pieces": pieces},
     return jsonify(response)
 
 
