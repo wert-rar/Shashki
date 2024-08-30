51880884b3bbbd4bc38f772c67d8a8e9c2801000
Христофор <J0muty@mail.ru>
2024-08-30 17:05:10 +0300

    Made a database where login, password and rank data can be entered

--- base.py
+++ base.py
@@ -3,9 +3,23 @@ import sqlite3
 con = sqlite3.connect("DataBase.db")
 cur = con.cursor()  # Для выполнения SQL-запросов
 
-cur.execute("CREATE TABLE IF NOT EXISTS Player("
-            "login TEXT, "
-            "password TEXT, "
-            "rang BIGINT)")
+cur.execute("""CREATE TABLE IF NOT EXISTS player(
+            login TEXT, 
+            password TEXT, 
+            rang BIGINT)""")
 
 con.commit()
+
+user_login = input('Login: ')
+user_password = input('Password: ')
+
+cur.execute("SELECT login FROM Player WHERE login = ?", (user_login,))
+if cur.fetchone() is None:
+    cur.execute("INSERT INTO Player VALUES (?, ?, ?)", (user_login, user_password, 0))
+    con.commit()
+    print('Пользователь успешно зарегистрирован!')
+else:
+    print('Такой пользователь уже зарегистрирован!')
+
+    for value in cur.execute("SELECT * FROM player"):
+        print(value)
