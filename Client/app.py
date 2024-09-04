32a9d5ac79bbbd847f3d9c06151abdbca39446f4
Христофор <J0muty@mail.ru>
2024-09-04 16:32:44 +0300

    Made registration and login pages and frontend for them

--- Client/app.py
+++ Client/app.py
@@ -118,6 +118,14 @@ def home():
 def get_board():
     return render_template('board.html')
 
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
 
 @app.route("/move", methods=["POST"])
 def move():
