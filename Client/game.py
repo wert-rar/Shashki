d0857aeb1c0a8edda12116382938d90119d4275c
wert-rar <72702040+wert-rar@users.noreply.github.com>
2024-09-13 00:54:11 +0300

    FIx game,py
    Delete useless import
    Relocate pieces and current player
    Added stubs for functions that should later be replaced with the code for Game array

--- Client/game.py
+++ Client/game.py
@@ -1,4 +1,32 @@
-from app import pieces
+current_player = "w"
+
+pieces = [
+    {"color": 1, "x": 1, "y": 0, "mode": "p"},
+    {"color": 1, "x": 3, "y": 0, "mode": "p"},
+    {"color": 1, "x": 5, "y": 0, "mode": "p"},
+    {"color": 1, "x": 7, "y": 0, "mode": "p"},
+    {"color": 1, "x": 0, "y": 1, "mode": "p"},
+    {"color": 1, "x": 2, "y": 1, "mode": "p"},
+    {"color": 1, "x": 4, "y": 1, "mode": "p"},
+    {"color": 1, "x": 6, "y": 1, "mode": "p"},
+    {"color": 1, "x": 1, "y": 2, "mode": "p"},
+    {"color": 1, "x": 3, "y": 2, "mode": "p"},
+    {"color": 1, "x": 5, "y": 2, "mode": "p"},
+    {"color": 1, "x": 7, "y": 2, "mode": "p"},
+    {"color": 0, "x": 0, "y": 7, "mode": "p"},
+    {"color": 0, "x": 2, "y": 7, "mode": "p"},
+    {"color": 0, "x": 4, "y": 7, "mode": "p"},
+    {"color": 0, "x": 6, "y": 7, "mode": "p"},
+    {"color": 0, "x": 1, "y": 6, "mode": "p"},
+    {"color": 0, "x": 3, "y": 6, "mode": "p"},
+    {"color": 0, "x": 5, "y": 6, "mode": "p"},
+    {"color": 0, "x": 7, "y": 6, "mode": "p"},
+    {"color": 0, "x": 0, "y": 5, "mode": "p"},
+    {"color": 0, "x": 2, "y": 5, "mode": "p"},
+    {"color": 0, "x": 4, "y": 5, "mode": "p"},
+    {"color": 0, "x": 6, "y": 5, "mode": "p"}
+]
+
 
 class Game:
     def __init__(self, f_user, c_user, game_id):
@@ -7,6 +35,7 @@ class Game:
         self.game_id = game_id
         self.moves_count = 0
         self.pieces = pieces
+        self.current_player = "w"
 
     def user_color(self, user_id):
         if user_id == self.f_user:
@@ -16,10 +45,23 @@ class Game:
         return None
 
     def pieces_and_current_player(self):
-        current_player = self.f_user if self.moves_count % 2 == 0 else self.c_user
+        self.current_player = self.f_user if self.moves_count % 2 == 0 else self.c_user
         current_color = 'white' if current_player == self.f_user else 'black'
         current_pieces = self.pieces[current_color]
         return current_player, current_pieces
 
     def __str__(self):
         return f"Game ID: {self.game_id}, White: {self.f_user}, Black: {self.c_user}"
+
+
+def find_waiting_game(game_id):
+    pass
+
+
+def update_game_with_user(game_id, user_login, color): pass
+
+
+def create_new_game(user_login): pass
+
+
+def get_game_status(game_id): pass
