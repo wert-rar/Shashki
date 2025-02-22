8566fab7e65133eeebb6f5884f9435cf7aef5eca
wert-rar <72702040+wert-rar@users.noreply.github.com>
2025-02-22 15:05:33 +0300

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 import sys, os
-sys.path.append('/home/j/j0mutyp2/thesashki.ru/Client/')
+sys.path.append('/home/j/j0mutyp2/thesashki.ru/thecheckers/')
 sys.path.append('/home/j/j0mutyp2/thesashki.ru/venv/lib/python3.10/site-packages')
 from app import app as application
